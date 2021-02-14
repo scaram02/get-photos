aec0f9c8604f6f79853dcf2d9a027cbcb2288684
Amelia Scarbrough <scarbroughm@hotmail.com>
2021-02-13 22:19:06 -0600

    fixed bug! and sort of styled, relatively

--- src/components/PhotoStream.js
+++ src/components/PhotoStream.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {useState} from 'react'
+import {useState, useEffect} from 'react'
 import '../App.css'
 
 const PhotoStream = () => {
@@ -18,44 +18,52 @@ const handleInputChange = (event) => {
     setQuery({...query, [name]: value})
 }
 
-
 const handleSubmit = e => {
     e.preventDefault()
-    console.log("Your query: ", query.query)
+    getPhotos(query.query)
+}
 
 
+const getPhotos = () => {
     var key = process.env.REACT_APP_FLICKR_KEY
     var flickrApi = ` https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${key}&tags=${query.query}&format=json&nojsoncallback=1`
     
-    console.log("I'm the api", flickrApi)
    fetch(flickrApi)
    .then((res) => {
        return res.json()
    })
-   .then((data => {
-       setPhotos(data)
-   }))
+   .then((data) => {
+       console.log(data)
+       const photoList = data.photos.photo.map((pic) => {
+            return <img key={pic.id} src={`https://live.staticflickr.com/${pic.server}/${pic.id}_${pic.secret}_w.jpg` || `https://forums.codemasters.com/uploads/monthly_2020_03/image.png.f8c83b98a2250b117a112bcfb92ca287.png`} alt={pic.title}/>
+        }) 
+    setPhotos(photoList)
+   })
 }
 
 
-const photoList = query.query && photos.photos.photo.map((pic) => {
-    return <img key={pic.id} src={`https://live.staticflickr.com/${pic.server}/${pic.id}_${pic.secret}_w.jpg` || `https://forums.codemasters.com/uploads/monthly_2020_03/image.png.f8c83b98a2250b117a112bcfb92ca287.png`} alt={pic.title}/>
-}) 
+
+
+
 
 
     return (
-        <div>
+    <div className="container">
+    <div className="search">
     <form onSubmit={handleSubmit}>
-      <label>search</label>
-      <input 
+    <input 
       type="text" 
       name="query" 
+      placeholder="search your favorite tags"
       value={query.query}
       onChange={handleInputChange} />
-      <button>Get photos!</button>
+      <button>Search tags!</button>
       </form>
-     {photoList}
-        </div>
+      </div>
+      <div className='image-container'>
+          {photos}
+     </div>
+    </div>
     )
 }
 
